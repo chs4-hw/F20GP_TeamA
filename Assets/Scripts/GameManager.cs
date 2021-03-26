4dcbe2ec1525fe396ff59ac8627639aae287e35d
chs4-hw <56627743+chs4-hw@users.noreply.github.com>
2021-03-26 16:32:42 +0000

    Added healthbar, spike traps & trap tilemap, game over screen.

---
+++ Assets/Scripts/GameManager.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class GameManager : MonoBehaviour
+{
+    // Start is called before the first frame update
+
+    bool playerDeath = false;
+    public float restartTimer = 1f;
+    public GameOverScreen gameOver;
+
+    public void EndGame()
+    {
+        if (playerDeath == false)
+        {
+            playerDeath = true;
+            gameOver.LoadScreen();
+
+            //Debug.Log("Game Over");
+            //Invoke("GameRestart", restartTimer);
+            //GameRestart();
+        }
+
+    }
+
+    //void GameRestart()
+    //{
+    //    SceneManager.LoadScene(SceneManager.GetActiveScene().name);
+    //}
+}
---
+++ Assets/Scripts/TrapObject.cs
@@ -0,0 +1,40 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.Tilemaps;
+using UnityEngine;
+
+[RequireComponent(typeof(TilemapCollider2D))]
+
+public class TrapObject : MonoBehaviour
+{
+
+    public int damage;
+    //Transform target;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        //target = PlayerManager.instance.player.transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    private void Reset()
+    {
+        GetComponent<TilemapCollider2D>().isTrigger = true;
+    }
+
+    private void OnTriggerEnter2D(Collider2D collision)
+    {
+        if(collision.tag =="Player")
+        {
+            Debug.Log($"{name} Triggered");
+            PlayerStatus health = collision.GetComponent<PlayerStatus>();
+            health.TakeHealthDamage(damage);
+        }
+    }
+}
