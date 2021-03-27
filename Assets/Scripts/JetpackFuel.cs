bf4024bfa4a017b6bb58e13e43ace91216b456cd
Fraser Simpson <fjs1@hw.ac.uk>
2021-03-27 02:35:24 +0000

    Jetpack Feature
    Added jetpack feature + UI.
    Multiple minor alterations to prevent jumping after jetpacking.
    Moved the traps tilemap to a separate layer so it's not considered ground (was re-enabling jumping when player took damage).
    Added new images for background, they wrap much better than original.
    Background now scrolls vertically as well as horizontally.
    Added UI visual indicators for jump, doublejump and jetpack fuel.

---
+++ Assets/Scripts/JetpackFuel.cs
@@ -0,0 +1,33 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+public class JetpackFuel : MonoBehaviour
+{
+    public Slider slider;
+    public int fuel = 100;
+
+    private int maxFuel = 500;
+    private bool jetpacking = false;
+
+    void Start() {
+        slider.maxValue = maxFuel;
+        slider.value = fuel;
+    }
+
+    void Update() {
+        if(Input.GetKey(KeyCode.Mouse1)) {
+            jetpacking = true;
+        }
+    }
+
+    void FixedUpdate() {
+        if(jetpacking) {
+            if(fuel > 0) {
+                fuel--;
+                slider.value = fuel;
+            }
+            jetpacking = false;
+        }
+    }
+
+}
