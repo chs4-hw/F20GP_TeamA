26ce4a132df0cca0afa7dac0fa92e8450cc5cafb
SuleDurrani <suledurrani1@gmail.com>
2021-03-27 20:18:35 +0000

    collectables
    added collectables and collectable scoreboard.

---
+++ Assets/CollectiblesScore.cs
@@ -0,0 +1,35 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CollectiblesScore : MonoBehaviour
+{
+
+    private int startCrystals;
+    private int x;
+    private int totalRemaning;
+    private GUIStyle guiStyle = new GUIStyle();
+    // Start is called before the first frame update
+    void Start()
+    {
+        startCrystals = GameObject.FindGameObjectsWithTag("Crystal").Length;
+        Debug.Log("awdawdawd" + startCrystals);
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        int currentCrystals = GameObject.FindGameObjectsWithTag("Crystal").Length;
+        x = startCrystals - currentCrystals;
+        totalRemaning = startCrystals - x;
+        //Debug.Log(x);
+    }
+
+    void OnGUI()
+    {
+        guiStyle.fontSize = 20;
+        guiStyle.normal.textColor = Color.white;
+        GUI.Label(new Rect(10, 10, 400, 20), "Crystals Collected: " + x, guiStyle);
+        GUI.Label(new Rect(10, 30, 400, 20), "Total Crystals in Level: " + totalRemaning, guiStyle);
+    }
+}
---
+++ Assets/Scripts/CrystalScript.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CrystalScript : MonoBehaviour
+{
+
+    public GameObject Character;
+    public float myX;
+    public float myY;
+    // Start is called before the first frame update
+    void Start()
+    {
+        myX = transform.position.x;                                                                        
+        myY = transform.position.y;
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        float x = Character.transform.position.x;                                                       
+        float y = Character.transform.position.y;
+
+        if ((x <= myX + 2.0f && x >= myX - 2.0f) && (y <= myY + 4.0f && y >= myY - 4.0f))
+        {
+            //Debug.Log("were touching ooo00oo0o");
+            Destroy(gameObject);
+        }
+        //else {
+            //Debug.Log("come closer");
+        //}
+    }
+
+
+    }
